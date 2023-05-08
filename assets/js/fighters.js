5c3a2033b8a32795b856e63b83232cc7b0a74b00
Barry Sheppard <barry.sheppard@gmail.com>
2023-05-09 00:17:03 +0100

    Added new runemark for Questor Soulsworn

--- assets/js/fighters.js
+++ assets/js/fighters.js
@@ -1577,6 +1577,8 @@ function getFactionRunemark(warband){
     else if(warband == "SCE:Thunderstrike") {runemark = "runemarks/white/factions-order-stormcast-eternals-thunderstrike.svg";}
     else if(warband == "SCE:Vanguard") {runemark = "runemarks/white/factions-order-stormcast-eternals-vanguard.svg";}
     else if(warband == "SCE:Warrior") {runemark = "runemarks/white/factions-order-stormcast-eternals-warrior.svg";}
+    else if(warband == "SCE:Questor Soulsworn") {runemark = "runemarks/white/factions-order-stormcast-eternals-questor-soulsworn.svg";}
+
     //else if(warband = "Chaos Legionnaaires") {runemark = "runemarks/white/factions-order-sylvaneth-alternative.svg";}
     else if(warband == "Sylvaneth") {runemark = "runemarks/white/factions-order-sylvaneth.svg";}
     else if(warband == "Askurgan Trueblades") {runemark = "runemarks/white/factions-death-askurgan-trueblades.svg";}
